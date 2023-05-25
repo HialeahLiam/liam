f8bb851f19ac152e86e6738bf41cd76e595071d8
Liam Idrovo <liamidrovo@gmail.com>
2023-05-24 18:01:23 -0700

    initial work

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  // ...
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // ...
+};
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brightOrange: "#FF470A",
+        mildOrange: "#D14836",
+      },
+      fontFamily: {
+        sans: ['"Helvetica Neue"', ...defaultTheme.fontFamily.sans],
+        body: ['"Josefin Sans Variable"', '"Josefin Sans"'],
+      },
+    },
+  },
+  plugins: [],
 };
